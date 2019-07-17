f772c67a72129e4cbedbbf060ca9c727c44df96f
Zhou <zrfanzy@gmail.com>
2019-07-17 10:15:15 +0200

    hide the token...

--- slack/send_message_example.py
+++ slack/send_message_example.py
@@ -1,11 +1,11 @@
 from slackclient import SlackClient
 
 def slack_message(message, channel):
-    token = 'xoxp-260448886196-259752523888-696536901092-017922652de4435bde7b97b4d75a88c0'
+    token = 'xxxxxxxxxxxxxxxxxxxxx'
     sc = SlackClient(token)    
 
     sc.api_call('chat.postMessage', channel=channel, 
                 text=message, username='My Sweet Bot',
                 icon_emoji=':robot_face:')
 
-slack_message('hello world from rzhou', 'random_discussions')
+slack_message('hello world', 'random_discussions')
